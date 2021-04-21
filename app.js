95c157b50ba3e92ab08a34919d2aad4285d71ab3
AYehia0 <ahmedyehia611@gmail.com>
2021-04-21 22:02:31 +0200

    main app/server

--- app.js
+++ app.js
@@ -1,2 +1,43 @@
 //MAIN APP
+const express = require('express');
+const path = require("path");
+const bodyParser = require('body-parser');
+const mongodb = require("mongoose");
+const User = require("./model/user");
 
+const PORT = 6969;
+
+//connecting the database, then creating the model itself to inforce the db schema 
+mongodb.connect("mongodb://localhost:27017/login-db", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex : true
+})
+
+const app = express();
+
+app.get('/', express.static(path.join(__dirname, "static")));
+
+//using the bodyparser with express
+app.use(bodyParser.json()); 
+//creating a new route to handle the post request of the register form 
+
+app.post('/register', async (req, res) => {
+
+    //showing the body of the resquest , to will return empty since express doesn't parse json 
+    console.log(req.body);
+
+
+    //hashing the password 
+
+
+    //if everthing is ok
+    res.json({status: "OK"})
+    
+})
+app.listen(PORT, (err) => {
+    if(!err)
+        console.log(`Listening on port: ${PORT}`);
+    else
+        console.log(err);
+})
